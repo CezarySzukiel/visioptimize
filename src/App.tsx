ab92a2c9d37e4664e1f9ef8098c764b8af929c6c
cezary <cezary.szukiel@gmail.com>
2024-10-25 11:20:43 +0200

    Basic configuration router and store

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useRoutes } from 'react-router-dom';
+import routes from './routes';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const element = useRoutes(routes);
+    return (
+        <div className="App">
+          <Header />
+          {element}
+          <Footer />
+        </div>
+    );
 }
 
 export default App;
